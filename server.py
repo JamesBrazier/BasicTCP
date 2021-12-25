603edfeebe3c994e8fc7554be2c4433aba4b02f5
James Brazier <49045066+JamesBrazier@users.noreply.github.com>
2021-12-26 12:04:12 +1300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from sys import argv
 from socket import *
 from datetime import datetime
+from time import sleep
 from shared import *
 
 def getPort():
@@ -19,54 +20,57 @@ def main():
     try:
         port = getPort()
 
+        server = None
         server = socket(AF_INET, SOCK_STREAM)
         server.bind(("", port))
         server.listen()
         server.setblocking(False)
         print(f"Server listening on port {port}...")
 
-    except OSError as err:
-        print(f"\n{err}\n")
-        exit()
+        while True:
+            try:
+                connection, address = server.accept()
+                with connection:
+                    connection.settimeout(1)
+                    print(f"Accepted connection from {addrToStr(address)} at {datetime.now()}")
 
-    while True:
-        try:
-            connection, address = server.accept()
-            with connection:
-                connection.settimeout(1)
-                print(f"Accepted connection from {addrToStr(address)} at {datetime.now()}")
+                    pkt = connection.recv(2048)
+                    file = unpackFileReq(pkt)
+                    print(f"Recieved file request from {addrToStr(address)} for {file}, reading...")
 
-                pkt = connection.recv(2048)
-                file = unpackFileReq(pkt)
-                print(f"Recieved file request from {addrToStr(address)} for {file}, reading...")
+                    try:
+                        with open(file, 'rb') as file:
+                            print("File successfully read")
 
-                try:
-                    with open(file, 'rb') as file:
-                        print("File successfully read")
+                            connection.send(createFileRes(file))
 
-                        connection.send(createFileRes(file))
-
-                        data = file.read(4096)
-                        connection.send(data)
-                        while len(data) != 0:
                             data = file.read(4096)
                             connection.send(data)
+                            while len(data) != 0:
+                                data = file.read(4096)
+                                connection.send(data)
 
-                except FileNotFoundError:
-                    print(f"File not found")
-                    connection.send(createFileRes(None))
+                    except FileNotFoundError:
+                        print(f"File not found")
+                        connection.send(createFileRes(None))
 
-                print(f"Response sent to {addrToStr(address)}...")
+                    print(f"Response sent to {addrToStr(address)}...")
 
-            print(f"Connection {addrToStr(address)} closed")
+                print(f"Connection {addrToStr(address)} closed")
 
-        except BlockingIOError:
-            wait(server, 0.1)
-            continue
+            except BlockingIOError:
+                sleep(server, 0.1)
+                continue
 
-        except (OSError, AppError) as err:
-            print(f"\nConnection error: {err}\n")
-            continue
+            except (OSError, AppError) as err:
+                print(f"\nConnection error: {err}\n")
+                continue
+        
+    except (OSError, KeyboardInterrupt) as err:
+        if socket != None:
+            socket.close()
+        print(f"\n{err}\n")
+        exit()
 
 
 main()
